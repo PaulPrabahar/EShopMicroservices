39e89d71c9bd3b4accd7b2c94b4ccfc674689832
Paul Prabahar <paul.prabahar@aravind.org>
2025-10-04 15:33:56 +0530

    code mismatch issue fixed

--- src/Service/Catalog/Catalog.Api/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Service/Catalog/Catalog.Api/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,15 +1,5 @@
-﻿using MediatR;
+﻿namespace Catalog.Api.Product.CreateProduct;
 
-namespace Catalog.Api.Product.CreateProduct;
-
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) :IRequest<CreateProductResult>;
-
-public record CreateProductResult(Guid Id);
-
-internal class CreateProductCommandEndpoint : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandEndpoint
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
 }
--- src/Service/Catalog/Catalog.Api/Product/CreateProduct/CreateProductHandler.cs
+++ src/Service/Catalog/Catalog.Api/Product/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,15 @@
-﻿namespace Catalog.Api.Product.CreateProduct;
+﻿using MediatR;
 
-public class CreateProductHandler
+namespace Catalog.Api.Product.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
